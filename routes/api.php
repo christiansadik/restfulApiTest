9e5d2c97f70ecb0f2df39bbf9723d8bf60f2f11e
CS_DEV <christian.sadikmelik@gmail.com>
2022-11-16 16:29:46 +0100

    Edit route API
    Add middleware CORS to route API

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::group(['prefix' => 'v1'], function(){
+Route::group(['middleware'=>'cors', 'prefix' => 'v1'], function(){
     Route::resource('users', UserController::class);
 });
