cce123a07cb3efe66f02dfdb4e52d1fab7cc840e
CS_DEV <christian.sadikmelik@gmail.com>
2022-11-17 23:58:18 +0100

    Bug fix

--- config/auth.php
+++ config/auth.php
@@ -41,7 +41,7 @@ return [
             'provider' => 'users',
         ],
 
-        'api' => [
+         'api' => [
             'driver' => 'passport', //update this line
             'provider' => 'users',
         ],
